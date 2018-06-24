715bfcbed2da0d4eea3f55a5dfe79f87f883acb2
José Naves <josenaves@gmail.com>
2018-06-24 14:51:32 -0300

    Lesson 2 - Explore screen

--- screens/Explore.js
+++ screens/Explore.js
@@ -6,10 +6,17 @@ import {
     SafeAreaView,
     TextInput,
     Platform,
-    StatusBar
+    StatusBar,
+    ScrollView,
+    Image,
+    Dimensions
 } from "react-native";
 
 import Icon from 'react-native-vector-icons/Ionicons'
+
+import Category from './components/Explore/Category';
+
+const {height, width} = Dimensions.get('window');
 
 class Explore extends Component {
 
@@ -39,6 +46,7 @@ class Explore extends Component {
                             marginTop: Platform.OS == 'android' ? 30 : null }}>
 
                             <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+
                             <TextInput
                                 placeholder="Try New Delhi"
                                 placeholderTextColor="grey"
@@ -46,6 +54,53 @@ class Explore extends Component {
                             />
                         </View>
                     </View>
+
+                    <ScrollView scrollEventThrottle={16}>
+                        <View style={{flex:1, backgroundColor: 'white', paddingTop: 20}}>
+                            <Text style={{ fontSize: 24, fontWeight:'700', paddingHorizontal: 20 }}>
+                                What can we help you find, Neto?
+                            </Text>
+                            <View style={{ height: 130, marginTop: 20 }}>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    <Category
+                                        name="Home"
+                                        imageUri={require('../assets/home.jpg')}
+                                    />
+
+                                    <Category
+                                        name="Experiences"
+                                        imageUri={require('../assets/experiences.jpg')}
+                                    />
+
+                                    <Category
+                                        name="Restaurant"
+                                        imageUri={require('../assets/restaurant.jpg')}
+                                    />
+                                </ScrollView>
+                            </View>
+
+                            <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+                                <Text style={{ fontSize: 24, fontWeight: '700'}}>
+                                    Introducing Airbnb Plus
+                                </Text>
+                                <Text style={{ fontWeight: '100', marginTop: 10}}>
+                                    A new selection of homes verified for qualitiy & confort
+                                </Text>
+
+                                <View style={{ width: width - 40, height: 200, marginTop: 20 }}> 
+                                    <Image 
+                                        style={{ flex: 1, height: null, width: null,
+                                            resizeMode: 'cover', borderRadius: 5, borderWidth: 1,
+                                            borderColor: '#dddddd'}}
+                                        source={ require('../assets/home.jpg') } />
+                                </View>
+
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </SafeAreaView>
         );
